a7ce1143eb5765e9303e9e3ea839c14d0bccaef9
YashGokhru <yashgokhru28@gmail.com>
2023-12-11 02:53:35 +0530

    Frontend : Create Post

--- News_Aggregator_2/frontend/src/components/Create.js
+++ News_Aggregator_2/frontend/src/components/Create.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 
 export class Create extends Component {
     constructor() {
-        super();
+        super(); 
         this.state = {
             title: "",
             content: "",
-            link: ""
+            link: "",
+            linkERR : ""
         }
+        this.text = "";
     }
-
+    
     handleTitleInput = (event) => {
         this.setState({
             title: event.target.value,
             content: this.state.content,
             link: this.state.link
         })
-        console.log(this.state);
+        // console.log(this.state);
     }
     handleContentInput = (event) => {
         this.setState({
@@ -24,22 +26,55 @@ export class Create extends Component {
             content: event.target.value,
             link: this.state.link
         })
-        console.log(this.state);
+        // console.log(this.state);
     }
     handleLinkInput = (event) => {
+        
         this.setState({
-            title: this.state.title,
+            title: this.state.title,    
             content: this.state.content,
             link: event.target.value
         })
-        console.log(this.state);
+        this.validateLink(event.target.value);
+    }
+    validateLink(link) {
+        
+        if(!link){
+            this.text = '';
+            return true;
+        }
+        const urlRegex = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
+    
+        if (urlRegex.test(link)) {
+            this.text = 'Valid URL with protocol';
+            return true;
+        } else {
+            this.text = 'Invalid URL or missing protocol';
+            return false;
+        }
+    }
+    handlePost = (event) => {
+        event.preventDefault();
+        if(!this.state.title){
+            alert("Title cannot be null");
+        }
+        else if (!this.state.content){
+            alert("Content cannot be null");
+        }
+        else if(this.state.link && !this.validateLink(this.state.link)){
+            
+        }
+        else{
+            alert("Posted !!");
+            
+        }
     }
 
     render() {
 
         return (
             <div className="container">
-                <div class="row justify-content-center my-4">
+                <div className="row justify-content-center my-4">
                     <div className="col-5 ">
                         <img src='CreateBig.svg'></img>
                     </div>
@@ -48,17 +83,20 @@ export class Create extends Component {
                             <label htmlFor="exampleFormControlInput1" className="form-label text-light">Post Title *</label>
                             <input onChange={this.handleTitleInput} className="form-control bg-dark text-light" id="exampleFormControlInput1" placeholder="Title" />
                         </div>
-                        <div className="mb-3">
+                        <div className="mb-4">
                             <label htmlFor="exampleFormControlTextarea1" className="form-label text-light">Post Content *</label>
-                            <textarea className="form-control bg-dark text-light" onChange={this.handleContentInput} id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <textarea className="form-control bg-dark text-light mb" onChange={this.handleContentInput} id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
                         <div className="mb-4 row justify-content-start">
                             <label htmlFor="link" className="col-sm-3 col-form-label text-light"> + Link </label>
                             <div className="col-sm-9 mb-4">
                                 <input className="form-control bg-dark text-light" id="link " onChange={this.handleLinkInput} />
+                                <div className='text-lgiht-muted' style={{color:'grey', height:'4px'}}><h7>{this.text}  </h7></div>
                             </div>
                         </div>
-                        <button type="button" class="btn btn-primary btn-lg">Post</button>
+                        
+                        <button type="submit" className="btn btn-primary btn-lg" onClick={this.handlePost}>Post</button>
+                        
                     </div>
                 </div>
             </div>
